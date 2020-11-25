837929bec497df9075cb8fafc7f62d20db2f730a
enrico <rombaldonienrico@gmail.com>
2020-11-25 09:09:05 +0100

    change comment

--- js/carousel.js
+++ js/carousel.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   //change image at click btn
+   //change image at click icon arrow
 
    $('.btn_next, .carousel_img> li').click(function(){
 
@@ -11,16 +11,16 @@ $(document).ready(function() {
      prevSlider();
 
    });
-
+    //change image at press keyboard right and left
    $(document).keydown(function(e){
 
      if(e.which==39 || e.keycode==39) nextSlider();
      else if (e.which==37 || e.keycode==37) prevSlider();
 
     });
-
+    //change image at click dots
     $('.dots> li').click( function(){
-      // var test
+
       dotsSliderClick($(this))
 
     });
@@ -57,7 +57,7 @@ function prevSlider() {
 
 }
 
-//change value at click dot
+//change image at click dot
 function dotsSliderClick(element){
 
   $('.carousel_img > li.active, .dots > li').removeClass('active');
