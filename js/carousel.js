009fac7e32e33463f482c627a700f25811f6252c
enrico <rombaldonienrico@gmail.com>
2020-11-25 01:37:50 +0100

    refactoring 2

--- js/carousel.js
+++ js/carousel.js
@@ -1,66 +1,67 @@
 $(document).ready(function() {
    //change image at click btn
+
    $('.btn_next, .carousel_img> li').click(function(){
+
      nextSlider();
 
    });
    $('.btn_prev').click(function(){
-     console.log("test");
+
      prevSlider();
+
    });
-    //change image at click dots;
-   //change image at press key arrow
+
    $(document).keydown(function(e){
-     if(e.which==39 || e.keycode==39){
-       nextSlider(imgActive,dotActive);
-     } else if (e.which==37 || e.keycode==37) {
-        prevSlider(imgActive, dotActive);
-     }
+
+     if(e.which==39 || e.keycode==39) nextSlider();
+     else if (e.which==37 || e.keycode==37) prevSlider();
+
     });
 
-        $('.dots> li').click( function(){
-          // var test
-        $('.carousel_img > li.active, .dots > li').removeClass('active');
-        $('.carousel_img > li').eq($(this).index()).addClass('active');
-        $('.dots > li').eq($(this).index()).addClass('active');
+    $('.dots> li').click( function(){
+      // var test
+      dotsSliderClick($(this))
 
-        });
+    });
 
   });
 
-//event to click btn right
+// change image at the click of the icon right
 function nextSlider() {
-  var imgNow=$('.carousel_img > li.active');
-  var dotNow=$('.dots> li.active');
-   if(imgNow.hasClass('last')){
-    imgNow.removeClass('active');
-    dotNow.removeClass('active');
-    $('.carousel_img > li.first').addClass('active');
-    $('.dots > li.first').addClass('active');
 
-  }else {
-    imgNow.removeClass('active');
-    dotNow.removeClass('active');
-    imgNow.next().addClass('active');
-    dotNow.next().addClass('active');
-  }
+   var positionNow=$('.carousel_img > li.active,.dots> li.active');
+   var positionFirst=  $('.carousel_img > li.first,.dots > li.first');
+   if(positionNow.hasClass('last')){
+     positionNow.removeClass('active');
+     positionFirst.addClass('active');
+   }else {
+       positionNow.removeClass('active');
+       positionNow.next().addClass('active');
+     }
 
 }
-//event to click btn left
-function prevSlider(){
-  var imgNow=$('.carousel_img > li.active');
-  var dotNow=$('.dots> li.active');
-  if(imgNow.hasClass('first')){
-   imgNow.removeClass('active');
-   dotNow.removeClass('active');
-   $('.carousel_img > li.last').addClass('active');
-  $('.dots > li.last').addClass('active');
 
- } else{
-  console.log('test');
-  imgNow.removeClass('active');
-  dotNow.removeClass('active');
-  imgNow.prev().addClass('active');
-  dotNow.prev().addClass('active');
-  }
+// change image at the click of the icon left
+function prevSlider() {
+
+  var positionNow=$('.carousel_img > li.active,.dots> li.active');
+  var positionLast=  $('.carousel_img > li.last,.dots > li.last');
+  if(positionNow.hasClass('first')){
+    positionNow.removeClass('active');
+    positionLast.addClass('active');
+  }else {
+      positionNow.removeClass('active');
+      positionNow.prev().addClass('active');
+    }
+
+}
+
+//change value at click dot
+function dotsSliderClick(element){
+
+  $('.carousel_img > li.active, .dots > li').removeClass('active');
+  $('.carousel_img > li').eq($(element).index()).addClass('active');
+  $('.dots > li').eq($(element).index()).addClass('active');
+
 }
